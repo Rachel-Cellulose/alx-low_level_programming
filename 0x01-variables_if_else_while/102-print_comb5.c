e0e34caf0a118c567b08936c2719908c763424a0
Rachel-Cellulose <adegboyegaseunfunmi98@gmail.com>
2022-09-11 07:56:11 -0700

    program that prints all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * codes for printing all possible combination of two d/t digits
+ * in assending order and separated by a comma followed by a space
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 100; digit1++)
+	{
+		for (digit2 = 0; digit2 < 100; digit2++)
+		{
+			if (digit2 > digit1)
+
+			{
+				putchar((digit1 / 10) + '0');
+				putchar((digit1 % 10) + '0');
+				putchar(' ');
+				putchar((digit2 / 10) + '0');
+				putchar((digit2 % 10) + '0');
+
+				if (digit1 != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
