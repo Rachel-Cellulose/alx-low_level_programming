397cee824751951aeb81857eafca61dd48d48c6a
Rachel-Cellulose <adegboyegaseunfunmi98@gmail.com>
2022-09-21 13:35:00 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,27 @@
 #include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: returns length as integer;
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
+
 /**
 *cap_string - function that capitalize first character of a word
 *@str: string to capitalize
 *Return:returns the capitalized string
 */
+
 char *cap_string(char *str)
 {
 	int index = 0;
@@ -28,5 +46,6 @@ char *cap_string(char *str)
 				str[index - 1] == '}')
 			str[index] -= 32;
 	}
+
 	return (str);
 }
